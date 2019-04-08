bae761d0adbd4896401fae9c928aed692a88c825
Codey Pinero <codey.pinero@gmail.com>
2019-04-08 01:48:54 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = [] 
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    @@count += 1
+    @@genres = [] << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+end
